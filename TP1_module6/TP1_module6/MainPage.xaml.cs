acfa7e4246106d8db35e6442c880b97fbc5cae08
Rudolph <rudolph.nativel@gmail.com>
2020-06-24 21:47:44 +0200

    Tp1_Module6

---
+++ TP1_module6/TP1_module6/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1_module6.Models;
+using Xamarin.Forms;
+
+namespace TP1_module6
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            new LoginForm(this.id, this.password, this.rebemerMe, this.loginF, this.tweet, this.errorLabel, this.ClickConnexion);
+        }
+
+        private void ClickConnexion_Ckicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TP1_module6/TP1_module6/Utils/ImageFromResources.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms.Xaml;
+
+using Xamarin.Forms;
+
+namespace TP1_module6.Utils
+{
+    [ContentProperty(nameof(Source))]
+    public class ImageFromResources : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageFromResources).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
