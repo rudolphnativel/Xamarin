6193d7f97093cc0658d2623e022d2eb436ca16e9
Rudolph <rudolph.nativel@gmail.com>
2020-06-24 22:05:58 +0200

    tp1_Module6 modif images

---
+++ TP1_Module7/TP1_Module7/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1_Module7.Models;
+using Xamarin.Forms;
+
+namespace TP1_Module7
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            new LoginForm(this.id, this.password, this.rebemerMe, this.loginF, this.tweet, this.errorLabel, this.ClickConnexion);
+        }
+    }
+}
---
+++ TP1_Module7/TP1_Module7/Models/VisibilitySwitch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TP1_Module7.Models
+{
+    class VisibilitySwitch
+    {
+        private View loginForm;
+        private View tweetForm;
+
+        public View InitialVisible { get; }
+        public View NextVisible { get; }
+
+        public VisibilitySwitch(View initialVisible, View nextVisible)
+        {
+            this.InitialVisible = initialVisible;
+            this.NextVisible = nextVisible;
+        }
+
+        public void Switch()
+        {
+            if (this.InitialVisible.IsVisible)
+            {
+                this.InitialVisible.IsVisible = false;
+                this.NextVisible.IsVisible = true;
+            }
+            else
+            {
+                this.InitialVisible.IsVisible = true;
+                this.NextVisible.IsVisible = false;
+            }
+        }
+    }
+}
